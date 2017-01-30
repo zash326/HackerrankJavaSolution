dbf4a948a4c1ef5f4b1df20aae711ec69fdd80e2
Ahadul Imam <zash326@gmail.com>
2017-01-30 02:02:52 -0800

    Submitted Code

---
+++ Submitted Code.java
@@ -0,0 +1,7 @@
+//Write your code here
+Set SET=new HashSet<String>();
+for (int i = 0; i < t; i++) {
+     
+            SET.add(pair_left[i]+" "+pair_right[i]);
+            System.out.println(SET.size());
+        }
