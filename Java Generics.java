020c6a46c37b291945fb130ecb41fbb78366f5bd
Ahadul Imam <zash326@gmail.com>
2017-01-30 02:04:54 -0800

    Java Generics

---
+++ Java Generics.java
@@ -0,0 +1,11 @@
+class Printer
+{
+   //Write your code here
+    public void printArray(Object S[]){
+        for(int i=0;i<S.length;i++){
+        System.out.println(S[i]);
+        }
+    }
+ 
+    }
+ 
