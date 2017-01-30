c5fbcf87b813d8f3e65ea9538ebeb4828f94baed
Ahadul Imam <zash326@gmail.com>
2017-01-30 00:41:53 -0800

    Java 1D Array

---
+++ Java 1D Array .java
@@ -0,0 +1,16 @@
+ Open in editor
+
+10
+        // Declare array a here
+11
+int a[]=new int[n];
+12
+        for(int i = 0 ; i < n; i++){
+13
+            int val = scan.nextInt();
+14
+            // Fill array a here
+15
+            a[i]=val;
+16
+        }
