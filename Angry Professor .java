0f4f9eea669e1228503aaf1177672dea7ecdf864
Ahadul Imam <zash326@gmail.com>
2017-01-30 17:09:25 -0800

    Angry Professor

---
+++ Angry Professor .java
@@ -0,0 +1,34 @@
+import java.io.*;
+import java.util.*;
+import java.text.*;
+import java.math.*;
+import java.util.regex.*;
+
+public class Solution {
+
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        int t = in.nextInt();
+        for(int a0 = 0; a0 < t; a0++){
+            int n = in.nextInt();
+            int k = in.nextInt();
+            int a[] = new int[n];
+            for(int a_i=0; a_i < n; a_i++){
+                a[a_i] = in.nextInt();
+            }
+            int sum=0;
+             for(int a_i=0; a_i < n; a_i++){
+               if(a[a_i]<=0){
+                   sum=sum+1;
+               }
+            }
+            if(sum<k){
+                System.out.println("YES");
+            }
+            else{
+              System.out.println("NO");  
+            }
+        }
+    }
+}
+
