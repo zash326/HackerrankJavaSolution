3fbf6d011de1bc1e42bef4edb7f8bbb0ee251758
Ahadul Imam <zash326@gmail.com>
2017-01-31 13:12:49 -0800

    Running Time of Algorithms

---
+++ Running Time of Algorithms.java
@@ -0,0 +1,55 @@
+import java.io.*;
+import java.util.*;
+import java.text.*;
+import java.math.*;
+import java.util.regex.*;
+
+public class Solution {
+public static void insertIntoSorted(int[] ar) {
+        int n=ar.length;
+        int temp=0;
+        int sum=0;
+        for(int i=1;i<n;i++){
+            temp=ar[i];
+             for(int j=i-1;j>=0;j--){
+                 
+                if(temp<ar[j]){
+                    sum=sum+1;
+                    ar[j+1]=ar[j];
+                    if(j==0){
+                        ar[0]=temp;
+                    }
+                }
+                else{
+                    ar[j+1]=temp;
+                    break;
+                
+                }
+                
+             }
+        }
+              System.out.println(sum);
+        
+       
+    }
+    
+    
+/* Tail starts here */
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        int s = in.nextInt();
+        int[] ar = new int[s];
+        for(int i=0;i<s;i++){
+            ar[i]=in.nextInt(); 
+        }
+        insertIntoSorted(ar);
+    }
+    
+    
+    private static void printArray(int[] ar) {
+        for(int n: ar){
+            System.out.print(n+" ");
+        }
+        System.out.println("");
+    }
+}
